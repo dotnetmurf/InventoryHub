8c2f57eb49b3c5daa3219747056ae8ca1f9dc9d3
dotnetmurf <dotnetmurf@outlook.com>
2025-10-31 17:17:28 -0500

    Update AppDbContext to use SharedModels

--- ServerApp/Data/AppDbContext.cs
+++ ServerApp/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ServerApp.Models;
+using SharedModels;
 
 namespace ServerApp.Data;
 
@@ -18,7 +19,7 @@ public class AppDbContext : DbContext
     /// <summary>
     /// Gets or sets the products in the database
     /// </summary>
-    public DbSet<Product> Products { get; set; }
+    public DbSet<ServerApp.Models.Product> Products { get; set; }
 
     /// <summary>
     /// Configures the database model
@@ -28,7 +29,7 @@ public class AppDbContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         // Configure Product entity
-        modelBuilder.Entity<Product>(entity =>
+        modelBuilder.Entity<ServerApp.Models.Product>(entity =>
         {
             entity.HasKey(e => e.Id);
             entity.Property(e => e.Name).IsRequired();
